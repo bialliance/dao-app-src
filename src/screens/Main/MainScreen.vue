a33e29cab4d9094800d952c13db0fbb29620f7e0
AlexStrakhov <astrahov92@me.com>
2021-04-19 22:05:13 +0500

    Дизайн главной страницы

--- src/screens/Main/MainScreen.vue
+++ src/screens/Main/MainScreen.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="dashboard2">
-    <div class="container">
-      <v-row justify="center" align="center">
-        <v-col><SvgIcon icon="logoBig"/></v-col>
-        <v-col cols="7">
-          <div class="dashboard2__content">
-            <p class="dashboard2__content-title">1clickDAO - Your personal DAO in 1 click </p>
-            <p class="dashboard2__content-text">DeFi ecosystem managed by DAO-interfaces provide a phenomenal combination of security, transparency, flexibility and cost-efficiency to asset managers and their clients.</p>
-          </div>
-        </v-col>
-      </v-row>
+    <div class="dashboard2">
+        <div class="container">
+            <v-row justify="center" align="center">
+                <v-col cols="12" md>
+                    <v-row justify="center">
+                        <v-col cols="auto">
+                            <SvgIcon class="dashboard2__logo" icon="logoBig" />
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col cols="11" md="9" lg="7">
+                    <div class="dashboard2__content">
+                        <p class="dashboard2__content-title">1clickDAO - Your personal DAO in 1 click </p>
+                        <p class="dashboard2__content-text">DeFi ecosystem managed by DAO-interfaces provide a phenomenal combination of security, transparency, flexibility and cost-efficiency to asset managers and their clients.</p>
+                    </div>
+                </v-col>
+            </v-row>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
     import SvgIcon from '@/features/SvgIcon'
+
     export default {
         components: {
             SvgIcon,
@@ -23,21 +30,29 @@
     }
 </script>
 
-<style lang='scss'>
-.dashboard2 {
-  padding-top: 100px;
-}
-.dashboard2__content-text {
-  font-size: 22px;
-  line-height: 30px;
-  text-align: left;
-}
-.dashboard2__content-title {
-  font-weight: bold;
-  margin-bottom: 25px;
-  font-size: 26px;
-  line-height: 30px;
-  text-align: left;
-}
+<style lang="scss">
+    .dashboard2 {
+        padding-top: 100px;
+
+        &__logo {
+            svg {
+                max-width: 100%;
+            }
+        }
+
+        &__content-text {
+            font-size:   22px;
+            line-height: 30px;
+            text-align:  left;
+        }
+
+        &__content-title {
+            font-weight:   bold;
+            margin-bottom: 25px;
+            font-size:     26px;
+            line-height:   30px;
+            text-align:    left;
+        }
+    }
 
 </style>
